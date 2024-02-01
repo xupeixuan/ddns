76b8b33dd33a485c361ca274644df51c72653a52
xupeixuan <29821630@qq.com>
2024-02-01 10:17:54 +0800

    extract constant to clean code

--- backend/src/main/java/ren/infox/ddns/client/IpifyClient.java
+++ backend/src/main/java/ren/infox/ddns/client/IpifyClient.java
@@ -9,9 +9,12 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Component
 public class IpifyClient {
 
+    private static final String USER_AGENT = "user-agent";
+    private static final String MOZILLA = "Mozilla";
+
     private final IpifyService ipifyService;
     public IpifyClient(@Value("${ipify.host}") String baseUrl) {
-        WebClient webClient = WebClient.builder().baseUrl(baseUrl).defaultHeader("user-agent", "Mozilla").build();
+        WebClient webClient = WebClient.builder().baseUrl(baseUrl).defaultHeader(USER_AGENT, MOZILLA).build();
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(WebClientAdapter.create(webClient)).build();
         ipifyService = httpServiceProxyFactory.createClient(IpifyService.class);
     }
